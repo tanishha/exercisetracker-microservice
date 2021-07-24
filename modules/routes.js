1aef5be3fba0db34061cff762ed86d6a75a514f3
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-24 09:56:14 +0545

    commit

--- modules/routes.js
+++ modules/routes.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 const Ctrl = require('./controller')
 
 Router.route("/users").post(Ctrl.postuser).get(Ctrl.getusers)
-Router.route("/users/:_id/exercises").post(Ctrl.postexercise)
-Router.route("/users/:_id/logs").get(Ctrl.logs)
+Router.route("/users/exercises").post(Ctrl.postexercise)
+Router.route("/users/logs").get(Ctrl.logs)
 
 module.exports = Router;
